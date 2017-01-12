5c61c5901fb0be19e36873f0a1e9bbff5e62f252
dapletch <seth.pletcher@gmail.com>
2017-01-11 19:59:14 -0500

    This is the initial commit for email jar file that I will attempt to create using the Spring framework.

---
+++ src/main/java/com/spring/sendemail/App.java
@@ -0,0 +1,11 @@
+package com.spring.sendemail;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println( "Hello World!" );
+    }
+}
